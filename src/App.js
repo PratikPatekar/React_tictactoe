fa00b6ec7e43c3f4d2360629541d0f48634cc876
Pratik Patekar <pratik.patekar3@gmail.com>
2020-12-04 13:04:24 +0530

    Updated package.json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Board from "./components/Board";
+import "./styles/root.scss";
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to React Parcel Micro App!</h1>
-      <p>Hard to get more minimal than this React app.</p>
-      <p>Hello!</p>
-    </>
+    <div className="app">
+      <h1>TIC TAC TOE!</h1>
+      <Board />
+    </div>
   );
 };
 
