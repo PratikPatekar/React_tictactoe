37134106f637ac2766324196c8040507c64d8ad5
Pratik Patekar <pratik.patekar3@gmail.com>
2020-12-04 16:08:14 +0530

    completed the game and added game history

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import React from "react";
-import Board from "./components/Board";
-import "./styles/root.scss";
+import React, { useState } from 'react';
+import Board from './components/Board';
+import History from './components/History';
+import './styles/root.scss';
+import { calculateWinner } from './helpers';
 
 const App = () => {
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), isXNext: true },
+  ]);
+
+  const [currentMove, setcurrentMove] = useState(0);
+
+  const current = history[currentMove];
+
+  const winner = calculateWinner(current.board);
+
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${current.isXNext ? 'X' : 'O'}`;
+
+  const handleSquareClick = position => {
+    if (current.board[position] || winner) {
+      return;
+    }
+    setHistory(prev => {
+      const last = prev[prev.length - 1];
+
+      const newBoard = last.board.map((square, pos) => {
+        if (pos === position) {
+          return last.isXNext ? 'X' : 'O';
+        }
+        return square;
+      });
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
+    });
+    setcurrentMove(prev => prev + 1);
+  };
+
+  const moveTo = move => {
+    setcurrentMove(move);
+  };
+
   return (
     <div className="app">
       <h1>TIC TAC TOE!</h1>
-      <Board />
+      <h2>{message}</h2>
+      <Board board={current.board} handleSquareClick={handleSquareClick} />
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
 };
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,25 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Square from './Square';
 
-const Board = () => {
-  const [board, setboard] = useState(Array(9).fill(null));
-  const [isXNext, setisXNext] = useState(false);
-
-  const handleSquareClick = position => {
-    if (board[position]) {
-      return;
-    }
-    setboard(prev => {
-      return prev.map((square, pos) => {
-        if (pos === position) {
-          return isXNext ? 'X' : 'O';
-        }
-        return square;
-      });
-    });
-    setisXNext(prev => !prev);
-  };
-
+const Board = ({ board, handleSquareClick }) => {
   const renderSquare = position => {
     return (
       <Square
